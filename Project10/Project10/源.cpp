7d49189ea1ccb77e7f2fdc88929bdde7915200de
process-that <1542099957@qq.com>
2022-03-01 22:10:29 +0800

    函数

---
+++ Project10/Project10/源.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+//int add(int num1, int num2) {
+//	//函数的定义
+//	int sum = num1 + num2;
+//	return sum;
+//}
+//int main() {
+//	//main函数调用add函数
+//	int a = 10;
+//	int b = 20;
+//	int c=add(a, b);
+//	cout << "c= " << c << endl;
+//	a = 500;
+//	b = 400;
+// c = add(a, b);
+//	cout << "c= " << c << endl;
+//	
+//	return 0;
+//}
+
+//值传递
+//void swap(int num1, int num2) {
+//	cout << "交换前" << endl;
+//	cout << "num1= " << num1 << endl;
+//	cout << "num2= " << num2 << endl;
+//	int temp = num1;
+//	num1 = num2;
+//	num2 = temp;
+//	cout << "交换后" << endl;
+//	cout << "num1= " << num1 << endl;
+//	cout << "num2= " << num2 << endl;
+//
+//}
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	cout << "a= " << a << endl;
+//	cout << "b= " << b << endl;
+//	swap(a, b);
+//	cout << "a= " << a << endl;
+//	cout << "b= " << b << endl;//形参发生改变不会影响实参
+//	return 0;
+//}
+
+//函数的声明
+//比较函数，实行两个整型数字进行比较，返回较大的值
+//int max(int a,int b);//函数的声明，可以有多次
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	cout << max(a, b) << endl;
+//	return 0;
+//}
+//int max(int a, int b) {
+//	return a > b ? a : b;
+//}
+//函数的分文件编写
+//1,创建后缀名为.h的头文件
+//2,创建后缀名为.cpp的源文件
+//3.在头文件中写函数的声明
+//4,在源文件中写函数的定义
+//void swap(int a, int b) {
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//	cout << "a= " << a << endl;
+//	cout << "b= " << b << endl;
+//}
+int main() {
+
+	return 0;
+}
